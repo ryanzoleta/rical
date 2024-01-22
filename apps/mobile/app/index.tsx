1aaae48f23cb79b88591b94f180d2515b7cd7361
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-01-23 01:15:32 +0800

    initial commit

---
+++ apps/mobile/app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+type Props = {};
+
+function HomeScreen({}: Props) {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
