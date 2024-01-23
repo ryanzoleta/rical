37b34851de3024e0e03c39b3881f4ab877f4d3e4
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-01-24 00:07:53 +0800

    initial UI

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,1 +1,33 @@
+import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import colors from 'tailwindcss/colors';
 import '../global.css';
+
+import React from 'react';
+import { useColorScheme } from 'react-native';
+
+type Props = {};
+
+function RootLayoutView({}: Props) {
+  const mode = useColorScheme();
+  const [fontsLoaded] = useFonts({
+    JetBrainsMono: require('../assets/fonts/JetBrainsMono-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack
+      screenOptions={{
+        contentStyle: {
+          backgroundColor: mode === 'dark' ? colors.zinc[900] : colors.zinc[100],
+        },
+        headerShown: false,
+      }}
+    ></Stack>
+  );
+}
+
+export default RootLayoutView;
--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { SafeAreaView, StatusBar, Text, TextInput, View, useColorScheme } from 'react-native';
+import { twMerge } from 'tailwind-merge';
 
 type Props = {};
 
 function HomeScreen({}: Props) {
+  const mode = useColorScheme();
+
+  StatusBar.setBarStyle(mode === 'dark' ? 'light-content' : 'dark-content');
+
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView
+      className={twMerge('min-h-screen', mode === 'dark' ? 'bg-zinc-900' : 'bg-zinc-100')}
+    >
+      <Text className="text-center text-xl">Rical</Text>
+
+      <TextInput className="font-jetBrainsMono text-2xl" />
+    </SafeAreaView>
   );
 }
 
