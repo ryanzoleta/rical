f6133a854ba7eee86eb1b0012ac412cfba837dd0
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-01-25 12:51:58 +0800

    syntax highlight of assignments and revision of implementation

--- apps/mobile/app/HomeScreen.tsx
+++ apps/mobile/app/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
 import { SafeAreaView, StatusBar, Text, TextInput, View, useColorScheme } from 'react-native';
 import { twMerge } from 'tailwind-merge';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -7,42 +7,48 @@ import { evaluate, isAssignment } from '../lib/evaluate';
 import { Variable, Result } from '../lib/types';
 
 export function HomeScreen({}: Props) {
-  const [input, setInput] = useState('');
+  const [inputList, setInputList] = useState(['']);
   const [outputs, setOutputs] = useState<Result[]>([]);
+  const [formattedInputList, setFormattedInputList] = useState<ReactNode[][]>([]);
+  // const [loaded, setLoaded] = useState(false);
+
+  const secondToTheLastInput = useRef<TextInput | null>(null);
 
   const mode = useColorScheme();
   StatusBar.setBarStyle(mode === 'dark' ? 'light-content' : 'dark-content');
 
-  useEffect(() => {
-    AsyncStorage.getItem('input').then((value) => setInput(value ?? ''));
-  }, []);
+  // useEffect(() => {
+  //   AsyncStorage.getItem('inputList').then((value) => setInputList(JSON.parse(value ?? '[""]')));
+  //   setLoaded(true);
+  // }, []);
+
+  // useEffect(() => {
+  //   if (loaded) AsyncStorage.setItem('inputList', JSON.stringify(inputList));
+  // }, [inputList]);
 
   useEffect(() => {
-    AsyncStorage.setItem('input', input);
-  }, [input]);
+    console.log(JSON.stringify(inputList));
 
-  useEffect(() => {
-    const lines = input.split('\n');
     const perLineOutput: Result[] = [];
     const variables: Variable[] = [];
 
-    for (const line of lines) {
+    for (const input of inputList) {
       let result;
 
-      if (line === '') {
+      if (input === '') {
         result = { raw: ' ', formatted: ' ' };
-      } else if (isAssignment(line)) {
-        result = evaluate(line.split('=')[1].trim(), variables);
-        variables.push({ name: line.split('=')[0].trim(), value: result.raw });
+      } else if (isAssignment(input)) {
+        result = evaluate(input.split('=')[1].trim(), variables);
+        variables.push({ name: input.split('=')[0].trim(), value: result.raw });
       } else {
-        result = evaluate(line.trim(), variables);
+        result = evaluate(input.trim(), variables);
       }
 
       perLineOutput.push(result);
     }
 
     setOutputs(perLineOutput);
-  }, [input]);
+  }, [inputList]);
 
   return (
     <SafeAreaView
@@ -59,15 +65,58 @@ export function HomeScreen({}: Props) {
 
       <View>
         <View className="flex-row justify-between">
-          <TextInput
-            className={twMerge(
-              'font-jetBrainsMono flex-1 px-3 text-2xl',
-              mode === 'dark' ? 'text-white' : 'text-black',
-            )}
-            multiline
-            value={input}
-            onChangeText={setInput}
-          />
+          <View className="flex-1">
+            {inputList.map((input, index) => (
+              <TextInput
+                key={index}
+                ref={index === inputList.length - 2 ? secondToTheLastInput : null}
+                className={twMerge(
+                  'font-jetBrainsMono px-3 text-2xl',
+                  mode === 'dark' ? 'text-white' : 'text-black',
+                )}
+                autoFocus
+                onSubmitEditing={() => {
+                  setInputList([...inputList, '']);
+                }}
+                onKeyPress={(event) => {
+                  if (event.nativeEvent.key === 'Backspace') {
+                    if (inputList[index] === '' && inputList.length > 1) {
+                      setInputList(inputList.slice(0, -1));
+                      if (secondToTheLastInput.current) {
+                        secondToTheLastInput.current.focus();
+                      }
+                    }
+                  }
+                }}
+                onChangeText={(text) => {
+                  const newInputList = [...inputList];
+                  newInputList[index] = text;
+                  setInputList(newInputList);
+
+                  const formattedInputLines = [];
+
+                  const lines = text.split('\n');
+
+                  for (const line of lines) {
+                    if (isAssignment(line)) {
+                      const tokens = line.split('=');
+                      formattedInputLines.push(<Text className="text-blue-500">{tokens[0]}=</Text>);
+                      formattedInputLines.push(<Text>{tokens[1]}</Text>);
+                    } else {
+                      formattedInputLines.push(line);
+                    }
+                  }
+
+                  const originalFormattedInputList = formattedInputList;
+                  originalFormattedInputList[index] = formattedInputLines;
+
+                  setFormattedInputList(originalFormattedInputList);
+                }}
+              >
+                <Text>{formattedInputList[index]}</Text>
+              </TextInput>
+            ))}
+          </View>
 
           <View>
             {outputs.map((output, index) => (
