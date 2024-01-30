868fe965a00afcd598d42df91cb154d38490ae32
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-01-30 15:44:46 +0800

    more ui tweaks

--- apps/mobile/app/settings.tsx
+++ apps/mobile/app/settings.tsx
@@ -2,21 +2,27 @@ import { ChevronRight, Sun, Hash, ChevronLeft } from 'lucide-react-native';
 import React from 'react';
 import { View, Text, SafeAreaView } from 'react-native';
 import colors from 'tailwindcss/colors';
-import HighlightedPressable from '../lib/components/PressableOpacity';
+import PressableOpacity from '../lib/components/PressableOpacity';
 import { router } from 'expo-router';
+import PressableBackground from '../lib/components/PressableBackground';
 
 function SettingsPage() {
   return (
     <SafeAreaView className="flex-col gap-5">
-      <HighlightedPressable className="flex-row items-center" onPress={router.back}>
+      <PressableOpacity className="flex-row items-center" onPress={router.back}>
         <ChevronLeft color={colors.blue[500]} size={30} />
         <Text className="text-xl text-blue-500">Back</Text>
-      </HighlightedPressable>
+      </PressableOpacity>
 
       <Text className="px-4 text-4xl font-bold">Settings</Text>
 
       <View className="gap-3">
-        <View className="mx-2 flex-row items-center justify-between rounded-lg bg-white p-4">
+        <PressableBackground
+          className="mx-2 flex-row items-center justify-between rounded-lg bg-white p-4"
+          onPress={() => {}}
+          defaultBackgrounrColor="white"
+          highlightBackgroundColor={colors.zinc[200]}
+        >
           <View className="flex-row gap-2">
             <Sun color={colors.zinc[400]} />
             <Text className="text-xl text-zinc-900">Appearance</Text>
@@ -26,9 +32,14 @@ function SettingsPage() {
             <Text className="text text-xl text-zinc-700">System</Text>
             <ChevronRight color={colors.zinc[400]} />
           </View>
-        </View>
+        </PressableBackground>
 
-        <View className="mx-2 flex-row items-center justify-between rounded-lg bg-white p-4">
+        <PressableBackground
+          className="mx-2 flex-row items-center justify-between rounded-lg bg-white p-4"
+          onPress={() => {}}
+          defaultBackgrounrColor="white"
+          highlightBackgroundColor={colors.zinc[200]}
+        >
           <View className="flex-row gap-2">
             <Hash color={colors.zinc[400]} />
             <Text className="text-xl text-zinc-900">Precision</Text>
@@ -38,7 +49,7 @@ function SettingsPage() {
             <Text className="text text-xl text-zinc-700">2 decimal points</Text>
             <ChevronRight color={colors.zinc[400]} />
           </View>
-        </View>
+        </PressableBackground>
       </View>
     </SafeAreaView>
   );
---
+++ apps/mobile/lib/components/PressableBackground.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from 'react';
+import { Pressable } from 'react-native';
+import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
+
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+
+type Props = {
+  children: ReactNode[];
+  defaultBackgrounrColor?: string;
+  highlightBackgroundColor?: string;
+  className?: string;
+  onPress: () => void;
+};
+
+function PressableBackground(props: Props) {
+  const backgroundColor = useSharedValue(props.defaultBackgrounrColor ?? 'white');
+
+  const handlePressIn = () => {
+    backgroundColor.value = withSpring(props.highlightBackgroundColor ?? 'white');
+  };
+
+  const handlePressOut = () => {
+    backgroundColor.value = withSpring(props.defaultBackgrounrColor ?? 'white');
+  };
+
+  return (
+    <AnimatedPressable
+      onPressIn={handlePressIn}
+      onPressOut={handlePressOut}
+      onPress={props.onPress}
+      style={{ backgroundColor }}
+      className={props.className}
+    >
+      {props.children}
+    </AnimatedPressable>
+  );
+}
+
+export default PressableBackground;
