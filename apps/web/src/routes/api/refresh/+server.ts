ff6b0e5c995d4f878b31a94989c95d6a32d5c59d
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2024-02-01 21:36:40 +0800

    better error handling

--- apps/web/src/routes/api/refresh/+server.ts
+++ apps/web/src/routes/api/refresh/+server.ts
@@ -1,18 +1,29 @@
 import { env } from '$env/dynamic/private';
 import moment from 'moment';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { db } from '$lib/db';
 import { rates } from '$lib/schema';
+import { error } from '@sveltejs/kit';
 
 export async function POST() {
-  const response = await axios.get<OpenExhangeRatesApiResponse>(
-    'https://openexchangerates.org/api/latest.json',
-    {
-      params: {
-        app_id: env.OPEN_EXCHANGE_RATES_APP_ID,
+  let response;
+
+  try {
+    response = await axios.get<OpenExhangeRatesApiResponse>(
+      'https://openexchangerates.org/api/latest.json',
+      {
+        params: {
+          app_id: env.OPEN_EXCHANGE_RATES_APP_ID + ':(',
+        },
       },
-    },
-  );
+    );
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      throw error(501, e?.response?.data.message ?? 'Unknown error');
+    }
+    console.error(e);
+    throw error(501, 'Unknown error');
+  }
 
   if (response.status === 200) {
     const rows = Object.entries(response.data.rates).map(([currency, value]) => ({
@@ -26,6 +37,8 @@ export async function POST() {
 
     return Response.json(await db.select().from(rates));
   }
+
+  throw error(501, response.statusText);
 }
 
 type OpenExhangeRatesApiResponse = {
